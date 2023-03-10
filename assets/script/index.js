b8a7bf6e18ad8b2501edd494784bb5bfe32ccb23
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-03-10 10:36:08 -0600

    Added time update funct and input validation

---
+++ assets/script/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// Declaring Elements
+const clock = document.querySelector('h1');
+const alarmTime = document.querySelector('span');
+const timeInp = document.querySelector('#time');
+const alarmBtn = document.querySelector('#set-alarm');
+
+// Time Update function
+function updateTime() {
+    let now = new Date();
+    let hour = now.getHours().toString().padStart(2, '0');
+    let minute = now.getMinutes().toString().padStart(2, '0');
+    let second = now.getSeconds().toString().padStart(2, '0');
+    
+    clock.innerText = `${hour}:${minute}:${second}`
+
+    // if statement
+};
+
+setInterval(updateTime, 1000);
+
+// Time Inp Validation
+timeInp.addEventListener('keyup', function() {
+    const validInp = /[^0-9:]/g;
+
+    if (validInp.test(this.value)) {
+        this.value = this.value.replace(validInp, '');
+    };
+});
+
+// Ser Alarm Function
+alarmBtn.addEventListener('click', function() {
+    const validTime = /^([0-1]?[0-9]|2[0-3]):([0-5][0-9])$/;
+
+    if (validTime.test(timeInp.value)) {
+        alarmTime.innerText = timeInp.value;
+    } else {
+        alarmTime.innerText = 'Please input a valid 24h time';
+    }
+})
